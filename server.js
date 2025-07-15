dacccf473da1fc5c3ef5b6f72c3e8feb8b51ddf8
MOHMMED.MOHIUDDIN <149958768+MDmohiuddin-web@users.noreply.github.com>
2025-07-15 21:30:37 +0600

    check

--- server.js
+++ server.js
@@ -1,35 +1,44 @@
-import express from 'express'
-import 'dotenv/config'
-import cors from 'cors'
-import connectDB from './configs/db.js';
-import adminRouter from './routes/adminRoutes.js';
-import blogRouter from './routes/blogRoutes.js';
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import adminRouter from "./routes/adminRoutes.js";
+import blogRouter from "./routes/blogRoutes.js";
 
 const app = express();
 
-await connectDB()
+await connectDB();
+
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN_URL_FOR_CLIENT || "*", // Allow all origins by default
+ 
+  credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: ["sessionId", "Content-Type", "Authorization"],
+  optionsSuccessStatus: 200,
+};
 
 // Middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors(corsOptions));
+app.use(express.json());
 
 // Routes
-app.get('/',(req,res)=> res.send("API is working"))
-app.use('/api/admin',adminRouter)
-app.use('/api/blog',blogRouter)
+app.get("/", (req, res) => res.send("API is working"));
+app.use("/api/admin", adminRouter);
+app.use("/api/blog", blogRouter);
 
 const PORT = process.env.PORT || 3000;
 
 function startServer() {
-    app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 }
 
 startServer();
 
-process.on('uncaughtException', (err) => {
-    console.log('Server crashed with error: ', err);
-    setTimeout(() => {
-        console.log('Restarting server...');
-        startServer();
-    }, 5000);
+process.on("uncaughtException", (err) => {
+  console.log("Server crashed with error: ", err);
+  setTimeout(() => {
+    console.log("Restarting server...");
+    startServer();
+  }, 5000);
 });
